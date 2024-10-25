f96231db122ed7d06773738411d1fff6a645027e
William Darian <williamdariansutedjo@gmail.com>
2024-10-25 17:59:01 +0900

    feat(2015/04): solved

---
+++ 2015/04-the-ideal-stocking-stuffer/src/main.rs
@@ -0,0 +1,33 @@
+use aoc::bootstrap;
+
+fn main() {
+  let (input, mut output) = bootstrap();
+  let input = input.trim();
+
+  let mut i = 0;
+  let nonce5 = loop {
+    let digest = md5::compute(format!("{input}{i}"));
+    let digest = format!("{:x}", digest);
+
+    if digest.starts_with("00000") {
+      break i;
+    }
+
+    i += 1;
+  };
+
+  let nonce6 = loop {
+    let digest = md5::compute(format!("{input}{i}"));
+    let digest = format!("{:x}", digest);
+
+    if digest.starts_with("000000") {
+      break i;
+    }
+
+    i += 1;
+  };
+
+  output.write_ln(&format!(
+    "Nonce (00000): {nonce5}\nNonce (000000): {nonce6}"
+  ));
+}
