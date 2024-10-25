8c48d017321e706dd29ae79c384771083c78efa6
William Darian <williamdariansutedjo@gmail.com>
2024-10-25 17:11:32 +0900

    feat(2015/03): solved

---
+++ 2015/03-perfectly-spherical-houses-in-a-vacuum/src/main.rs
@@ -0,0 +1,55 @@
+use std::collections::HashSet;
+
+use aoc::bootstrap;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Pos(usize, usize);
+
+impl Pos {
+  pub fn act(&mut self, char: char) {
+    match char {
+      | '^' => self.1 += 1,
+      | 'v' => self.1 -= 1,
+      | '<' => self.0 -= 1,
+      | '>' => self.0 += 1,
+      | _ => {},
+    }
+  }
+}
+
+fn main() {
+  let (input, mut output) = bootstrap();
+
+  // Part 1
+  let mut position = Pos(0, 0);
+  let mut visited = HashSet::<Pos>::new();
+  visited.insert(position);
+
+  for c in input.chars() {
+    position.act(c);
+    visited.insert(position);
+  }
+
+  let part1 = visited.len();
+
+  // Part 2
+  let mut positions = (Pos(0, 0), Pos(0, 0));
+  visited.clear();
+  visited.insert(positions.1);
+
+  for (i, c) in input.chars().enumerate() {
+    if i % 2 == 0 {
+      positions.0.act(c);
+      visited.insert(positions.0);
+    } else {
+      positions.1.act(c);
+      visited.insert(positions.1);
+    }
+  }
+
+  let part2 = visited.len();
+
+  output.write_ln(&format!(
+    "Santa Alone: {part1}\nSanta and Robo-Santa: {part2}"
+  ));
+}
