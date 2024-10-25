067f12265cccd56e924177ce278034a51cc3302c
William Darian <williamdariansutedjo@gmail.com>
2024-10-25 16:29:09 +0900

    feat(2015/01): part 2

--- 2015/01-not-quite-lisp/src/main.rs
+++ 2015/01-not-quite-lisp/src/main.rs
@@ -4,13 +4,20 @@ fn main() {
   let (input, mut output) = bootstrap();
 
   let mut floor = 0;
-  for c in input.chars() {
+  let mut basement = std::usize::MAX;
+  for (i, c) in input.chars().enumerate() {
     match c {
       | '(' => floor += 1,
       | ')' => floor -= 1,
       | _ => {},
     }
+
+    if floor < 0 && (i + 1) < basement {
+      basement = i + 1
+    }
   }
 
-  output.write_ln(&format!("{floor}"));
+  output.write_ln(&format!(
+    "Floor: {floor}\nEntered basement at step: {basement}"
+  ));
 }
