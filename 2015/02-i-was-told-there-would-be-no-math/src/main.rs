7c877d4365076301f97cb69116303a1d0ba31c52
William Darian <williamdariansutedjo@gmail.com>
2024-10-25 16:47:34 +0900

    feat(2015/02): solved

---
+++ 2015/02-i-was-told-there-would-be-no-math/src/main.rs
@@ -0,0 +1,33 @@
+use aoc::bootstrap;
+
+fn main() {
+  let (input, mut output) = bootstrap();
+
+  let mut area = 0;
+  let mut ribbon = 0;
+  for line in input.split("\n") {
+    if line == "" {
+      continue;
+    }
+
+    let mut measurements = line
+      .split("x")
+      .map(|v| v.parse::<usize>().unwrap())
+      .collect::<Vec<_>>();
+    measurements.sort();
+    let x = measurements[0];
+    let y = measurements[1];
+    let z = measurements[2];
+
+    let a = x * y;
+    let b = y * z;
+    let c = x * z;
+
+    let slack = a.min(b).min(c);
+
+    area += 2 * (a + b + c) + slack;
+    ribbon += 2 * (x + y) + (x * y * z);
+  }
+
+  output.write_ln(&format!("Area: {area}\nRibbon: {ribbon}"));
+}
